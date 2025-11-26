bf20a51b2051ae03c59f7f76154d4aa186f6dcd4
uswamaryam <uswamaryam.se@gmail.com>
2025-11-26 14:19:52 +0500

    uswa changes

--- server/server.js
+++ server/server.js
@@ -31,7 +31,7 @@ connectDB();
 app.get('/', (req, res) => {
   res.json({
     status: 'ok',
-    message: 'Talentro API Server',
+    message: 'Talentro Platform Server',
     version: '1.0.0',
     timestamp: new Date().toISOString(),
   });
